1b6b9ff660acd4637c61851d30a9246e10741108
Dilethni <163492301+Dilethni@users.noreply.github.com>
2026-03-12 20:22:58 +0530

    Initial commit - Smart Campus API

---
+++ src/main/java/com/smartcampus/Main.java
@@ -0,0 +1,42 @@
+package com.smartcampus;
+
+import com.smartcampus.filter.LoggingFilter;
+import com.smartcampus.resource.DiscoveryResource;
+import com.smartcampus.resource.RoomResource;
+import com.smartcampus.resource.SensorResource;
+import com.smartcampus.exception.*;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJsonProvider;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+
+public class Main {
+    public static final String BASE_URI = "http://localhost:8080/api/v1/";
+
+    public static void main(String[] args) throws Exception {
+        final ResourceConfig rc = new ResourceConfig()
+                // Register resources manually
+                .register(DiscoveryResource.class)
+                .register(RoomResource.class)
+                .register(SensorResource.class)
+                // Register exception mappers
+                .register(RoomNotEmptyExceptionMapper.class)
+                .register(LinkedResourceNotFoundExceptionMapper.class)
+                .register(SensorUnavailableExceptionMapper.class)
+                .register(GlobalExceptionMapper.class)
+                // Register filter
+                .register(LoggingFilter.class)
+                // Register Jackson JSON
+                .register(JacksonJsonProvider.class);
+
+        final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
+                URI.create(BASE_URI), rc);
+
+        System.out.println("Smart Campus API running at " + BASE_URI);
+        System.out.println("Press ENTER to stop...");
+        System.in.read();
+        server.stop();
+    }
+}
